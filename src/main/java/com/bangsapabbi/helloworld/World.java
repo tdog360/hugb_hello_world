50fd4470a4ed15894c8e73e357bb39f8ca2b10a5
Tinna Dis Sigmarsdottir <tinna@i-1217c9a8.cs1004cloud.internal>
2016-10-23 19:38:08 +0000

    step 4 almost finished, need 2 fix dependencies

--- src/main/java/com/bangsapabbi/helloworld/World.java
+++ src/main/java/com/bangsapabbi/helloworld/World.java
@@ -1,10 +1,17 @@
 package com.bangsapabbi.helloworld;
 
+import net.joningi.icndb.ICNDBClient; //Init
+import net.joningi.icndb.Joke; //Get single joke by ID
+
+
 public class World {
 
-    private String greeting = "Hello world!";
+    //private String greeting = "Hello world!"; 
+    final ICNDBClient client = new ICNDBClient();
 
     public String greet() {
-        return greeting;
+        //return greeting;
+	Joke randJoke = client.getRandom();
+	return randJoke.getJoke();
     }
 }
--- src/test/java/com/bangsapabbi/helloworld/WorldTest.java
+++ src/test/java/com/bangsapabbi/helloworld/WorldTest.java
@@ -1,7 +1,8 @@
 package com.bangsapabbi.helloworld;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 import org.junit.Test;
 
 public class WorldTest {
@@ -9,6 +10,8 @@ public class WorldTest {
     @Test
     public void greetResultsInHello() {
         World world = new World();
-        assertEquals("Hello world!", world.greet()); }
+      //  assertEquals("Hello world!", world.greet()); 
+	assertThat(world.greet(), containsString("Chuck Norris"));
+    }
 
 }
